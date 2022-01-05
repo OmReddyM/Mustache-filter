c91579dc47e8b89782f54da8d4e513afe54f8ddc
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2022-01-05 16:45:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-function preload() { }
+noseX = 0;
+noseY = 0;
+function preload() {
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
     video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -10,6 +15,7 @@ function setup() {
 }
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 70, 35);
 }
 
 function modelLoaded() {
@@ -19,8 +25,10 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("Nose X = ", results[0].pose.nose.x);
-        console.log("Nose Y = ", results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 35;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = ", noseX);
+        console.log("Nose Y = ", noseY);
     }
 }
 
